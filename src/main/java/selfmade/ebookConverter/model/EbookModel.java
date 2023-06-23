1744e7d9db6b4d32f485be0d1e2d13dbd6c7a25b
DJGofficial <120133080+DJGofficial@users.noreply.github.com>
2023-06-23 20:50:55 +0200

    Implemented FileChooser

--- src/main/java/selfmade/ebookConverter/model/EbookModel.java
+++ src/main/java/selfmade/ebookConverter/model/EbookModel.java
@@ -1,30 +1,22 @@
 package selfmade.ebookConverter.model;
 
-import javafx.scene.control.TreeItem;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class EbookModel {
 
-    public static TreeItem<String> createDirectoryTree(File directory) {
-        System.out.println("createDirectoryTree(File directory)"+directory.getName());
-        TreeItem<String> rootItem = new TreeItem<>(directory.getName());
-        rootItem.setExpanded(true);
-
-        createTree(directory, rootItem);
-
-        return rootItem;
-    }
-
-    private static void createTree(File file, TreeItem<String> parentItem) {
-        System.out.println("createTree(File file, TreeItem<String> parent");
-        if (file.isDirectory()) {
-            for (File subFile : file.listFiles()) {
-                TreeItem<String> newItem = new TreeItem<>(subFile.getName());
-                System.out.println(newItem.toString());
-                parentItem.getChildren().add(newItem);
-                createTree(subFile, newItem);
+    public static void readStart(File file) {
+        try (
+                FileReader fr = new FileReader(file)) {
+            int content;
+            while ((content = fr.read()) != -1) {
+                System.out.print((char) content);
             }
+        } catch (
+                IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/selfmade/ebookConverter/view/EbookView.java
+++ src/main/java/selfmade/ebookConverter/view/EbookView.java
@@ -4,93 +4,43 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import selfmade.ebookConverter.model.EbookModel;
 
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import selfmade.ebookConverter.model.EbookModel;
 
 
 public class EbookView extends HBox implements Initializable {
     @FXML
-    private TreeView<File> treeView = buildFileSystemBrowser();
+    private TreeView<File> treeView;
+    @FXML
+    Button fileButton = new Button("Choose File");
+    @FXML
+    TextField fileTextField= new TextField();
     File directory = new File("/home/usr/");
 
-    public EbookView() {
-        //   this.treeView = treeView;
-        //     getChildren().add(treeView);
-        //   treeView.setRoot(EbookModel.createDirectoryTree(directory));
-    }
+    FileChooser fileChooser = new FileChooser();
 
     @FXML
-    protected void selectItem() {
+    void selectFile(MouseEvent event) {
+        File file = fileChooser.showOpenDialog(new Stage());
+        fileTextField.setText(file.getName());
+        EbookModel.readStart(file);
+
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        TreeView<File> treeView = buildFileSystemBrowser();
-       // TreeItem<String> rootItem = new TreeItem<>(directory.getName());
-       // treeView.setRoot(rootItem);
+        fileChooser.setInitialDirectory(new File("/"));
 
     }
 
-    private TreeView buildFileSystemBrowser() {
-        TreeItem<File> root = createNode(new File("/"));
-        return new TreeView<File>(root);
-    }
-
-    private TreeItem<File> createNode(final File f) {
-        return new TreeItem<File>(f) {
-            private boolean isLeaf;
-
-            private boolean isFirstTimeChildren = true;
-            private boolean isFirstTimeLeaf = true;
-
-            @Override
-            public ObservableList<TreeItem<File>> getChildren() {
-                if (isFirstTimeChildren) {
-                    isFirstTimeChildren = false;
-
-                    // First getChildren() call, so we actually go off and
-                    // determine the children of the File contained in this TreeItem.
-                    super.getChildren().setAll(buildChildren(this));
-                }
-                return super.getChildren();
-            }
-
-            @Override
-            public boolean isLeaf() {
-                if (isFirstTimeLeaf) {
-                    isFirstTimeLeaf = false;
-                    File f = (File) getValue();
-                    isLeaf = f.isFile();
-                }
-
-                return isLeaf;
-            }
-
-            private ObservableList<TreeItem<File>> buildChildren(TreeItem<File> TreeItem) {
-                File f = TreeItem.getValue();
-                if (f != null && f.isDirectory()) {
-                    File[] files = f.listFiles();
-                    if (files != null) {
-                        ObservableList<TreeItem<File>> children = FXCollections.observableArrayList();
-
-                        for (File childFile : files) {
-                            children.add(createNode(childFile));
-                        }
-
-                        return children;
-                    }
-                }
-
-                return FXCollections.emptyObservableList();
-            }
-        };
-    }
 }
