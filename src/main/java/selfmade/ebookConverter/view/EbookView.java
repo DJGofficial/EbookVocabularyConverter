12b11e0b3a3ea2b4645d68a43a7b3acccb922a45
DJGofficial <120133080+DJGofficial@users.noreply.github.com>
2023-06-24 20:34:07 +0200

    New Design elements, start creating file creator

--- src/main/java/selfmade/ebookConverter/view/EbookView.java
+++ src/main/java/selfmade/ebookConverter/view/EbookView.java
@@ -1,12 +1,11 @@
 package selfmade.ebookConverter.view;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
+
+
 import javafx.scene.layout.HBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -15,22 +14,24 @@ import selfmade.ebookConverter.model.EbookModel;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
-import selfmade.ebookConverter.model.EbookModel;
 
 
 public class EbookView extends HBox implements Initializable {
-    @FXML
-    private TreeView<File> treeView;
+
     @FXML
     Button fileButton = new Button("Choose File");
+
     @FXML
-    TextField fileTextField= new TextField();
-    File directory = new File("/home/usr/");
+    Button createButton = new Button();
+    @FXML
+    Button ankiButton = new Button();
+    @FXML
+    TextField fileTextField = new TextField();
 
     FileChooser fileChooser = new FileChooser();
 
     @FXML
-    void selectFile(MouseEvent event) {
+    void selectFile() {
         File file = fileChooser.showOpenDialog(new Stage());
         fileTextField.setText(file.getName());
         EbookModel.readStart(file);
