72b20257054e4c8e004d89158ced2c6789660aea
DJGofficial <120133080+DJGofficial@users.noreply.github.com>
2023-08-08 12:46:51 +0200

    Switch-Case finished, try to add blocks to ArrayList with endMark as stop

--- src/main/java/selfmade/ebookConverter/controller/TrimAlgorithm.java
+++ src/main/java/selfmade/ebookConverter/controller/TrimAlgorithm.java
@@ -11,15 +11,15 @@ import java.util.List;
 public class TrimAlgorithm {
 
     private String title = "";
-    private String type = "";
+    private StringBuilder titleToAdd = new StringBuilder();
+    private String type = titleToAdd.toString();
     private String endMark = "";
     private String vocabulary = "";
     private ObservableList<Node> contentList;
 
     private List<String> texts = new ArrayList<>();
-    private List<String> textSeparated = new ArrayList<>();
+    private List<String> entryList = new ArrayList<>();
 
-    private StringBuilder textToAdd = new StringBuilder();
 
     public TrimAlgorithm(ObservableList<Node> content) {
 
@@ -32,11 +32,61 @@ public class TrimAlgorithm {
         for (Node node : contentList) {
             ToggleButton toggleButton = (ToggleButton) node;
             String buttonText = toggleButton.getText();
-            texts.add(buttonText);
+            texts.add(buttonText + " ");
         }
-        //findAndPrintSpecialEntries();
         getMarkings();
     }
+
+    //Search content for begin/end marks
+    private void getMarkings() {
+        for (Node node : contentList) {
+            ToggleButton toggleButton = (ToggleButton) node;
+            String buttonText = toggleButton.getText();
+            String styleString = node.lookup(".toggle-button").getStyle();
+            switch (styleString) {
+                case "-fx-background-color: #D9FFD9":
+                    endMark = buttonText;//text before endMark is added as unit
+                    break;
+                case "-fx-background-color: #FFD9D9":
+                    vocabulary = buttonText;  //word that is later translated
+                    break;
+                case "-fx-background-color: #FFFFD9":
+                    titleToAdd.append(buttonText + " ");//Only include books with this title
+                    //ATTENTION SPACE COULD BE ADDED AFTER LAST POSITION
+                    break;
+                case "-fx-background-color: #D9E5FF":
+                    type = buttonText;//Only include text with this type
+                    break;
+            }
+        }
+        extractVocabulary(endMark, vocabulary, titleToAdd, type);
+        divideBlocksAddToList(endMark);
+    }
+
+    private void extractVocabulary(String endMark, String vocabulary, StringBuilder titleToAdd, String type) {
+        System.out.print(endMark + " " + vocabulary + " " + titleToAdd + " " + type);
+        for (String text : texts) {
+            //    System.out.print(text);
+        }
+    }
+
+    private void divideBlocksAddToList(String endMark) {
+        StringBuilder entryBuilder = new StringBuilder();
+
+        for (String text : texts) {
+            if (text.equals(endMark)) {
+                entryList.add(entryBuilder.toString());
+                entryBuilder.setLength(0);
+            } else {
+                entryBuilder.append(text).append(" ");
+            }
+        }
+        if (entryBuilder.length() > 0) {
+            entryList.add(entryBuilder.toString());
+        }
+
+    }
+
 
     private void findAndPrintSpecialEntries() {
         /*for (String text : texts) {
@@ -46,77 +96,6 @@ public class TrimAlgorithm {
         }
          */
 
-    }
-
-    //Search content for begin/end marks
-    private void getMarkings() {
-        System.out.println("GetMarking wurde aufgerufen");
-        for (Node node : contentList) {
-            ToggleButton toggleButton = (ToggleButton) node;
-            String buttonText = toggleButton.getText();
-            String styleString = node.lookup(".toggle-button").getStyle();
-            switch (styleString) {
-                case "-fx-background-color: #D9FFD9":
-                    endMark = buttonText;//text before endMark is added as unit
-                    System.out.println(endMark);
-                case "-fx-background-color: #FFD9D9":
-                    vocabulary = buttonText;  //word that is later translated
-                    System.out.println(vocabulary);
-                case "-fx-background-color: #FFFFD9":
-                    title = buttonText;//Only include books with this title
-                    System.out.println(title);
-                case "-fx-background-color: #D9E5FF":
-                    type = buttonText;//Only include text with this type
-                    System.out.println(type);
-
-            }
-        }
-            extractVocabulary();
-    }
-
-    private void extractVocabulary() {
-        System.out.println("You arte heere");
-    }
-
-    private void separatTextFragments(String value) {
-        for (String text : texts) {
-            if (text.equals(value)) {
-            }
-                 /*
-                // Wenn die aktuelle Zeile die Trennzeile ist, fügen Sie den gespeicherten Text zur Liste hinzu
-                if (!textSeparated.isEmpty()) {
-                    System.out.println("Arrived");
-                    String savedText = String.join("\n", textSeparated); // Die Texte mit Zeilenumbruch verbinden
-                    System.out.println("Gespeicherter Text: \n" + savedText);
-                    textSeparated.clear(); // Liste leeren, um für den nächsten gespeicherten Text bereit zu sein
-                }
-            } else {
-                // Wenn die aktuelle Zeile kein Trennzeichen ist, fügen Sie sie zur Liste hinzu
-                System.out.println(text);
-                textSeparated.add(text);
-            }
-            for (String texter : textSeparated) {
-                System.out.println(text);
-            }
-        }
-
-
-            for (int i = 0; i < texts.size() ; i++) {
-                String currentText = textSeparated.get(i);
-                if (currentText.contains(value)) {
-                    // Wenn der Wert gefunden wurde, fügen Sie den vorherigen Textbaustein (falls vorhanden) zur extrahierten Liste hinzu.
-                    if (i > 0) {
-                        String previousText = texts.get(i - 1);
-                        textSeparated.add(previousText);
-                    }
-                }
-            }
-            for (String extractedText : textSeparated) {
-                System.out.println(extractedText);
-            }
-
-             */
-        }
     }
 
 }
